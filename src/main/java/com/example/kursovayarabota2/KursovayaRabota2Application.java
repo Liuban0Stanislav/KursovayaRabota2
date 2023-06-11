d66c179ef695839a69e3e31875b619d2c904f96b
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-06-11 15:02:23 +1000

    init commit

---
+++ src/main/java/com/example/kursovayarabota2/KursovayaRabota2Application.java
@@ -0,0 +1,13 @@
+package com.example.kursovayarabota2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursovayaRabota2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursovayaRabota2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kursovayarabota2/KursovayaRabota2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursovayarabota2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursovayaRabota2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
